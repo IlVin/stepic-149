ba729870cf723395a2e1db228dacfdec4d3d0db2
Ilia Vinokurov <ilvin@mail.ru>
2016-02-17 14:43:09 +0300

    Кастомный парсер выражения.

--- proizvod/main.cpp
+++ proizvod/main.cpp
@@ -1,36 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
 struct TOK {
-    int sign;
     int k;
-    int x;
     int p;
 };
-        ///////////////////////////////////////////////////////
-        // <expression> ::= <first term> [ <addop> <term> ]* //
-        // <first term> ::= <first factor> <rest>            //
-        // <term> ::= <factor> <rest>                        //
-        // <rest> ::= [ <mulop> <factor> ]*                  //
-        // <first factor> ::= [ <addop> ] <factor>           //
-        // <factor> ::= <base> [ ^ <factor> ]                //
-        // <base> ::= <var> | <number> | ( <expression> )    //
-        ///////////////////////////////////////////////////////
-
-        ///////////////////////////////////////////////////////////////////////
-        // <expression> ::= <first term> [ <addop> <term> ]*                 //
-        // <first term> ::= <first factor> <rest>                            //
-        // <term> ::= <factor> <rest>                                        //
-        // <rest> ::= [ <mulop> <factor> ]*                                  //
-        // <first factor> ::= [ <addop> ] <factor>                           //
-        // <factor> ::= <number> | <var> [^ <number>]* | ( <expression> )    //
-        ///////////////////////////////////////////////////////////////////////
-
-
 
 class ExParser {
     private:
@@ -39,72 +18,71 @@ class ExParser {
         int pos;
 
         bool op(char symbol) {
+            while(s[pos] == ' ') pos++;
             if (s[pos] == symbol) {
-                std::cout << "s[" << pos << "] == " << symbol << std::endl;
                 pos++;
                 return true;
             }
             return false;
         }
 
-        bool addop()    { while(op(' ')); return op('-') || op('+'); }
-        bool mulop()    { while(op(' ')); return op('*') || op('/'); }
-        bool powop()    { while(op(' ')); return op('^'); }
-        bool var()      { while(op(' ')); return op('x'); }
-        bool rbr_l()    { while(op(' ')); return op('('); }
-        bool rbr_r()    { while(op(' ')); return op(')'); }
-
-        bool number()  {
-            std::cout << "number" << std::endl;
-            while(op(' '));
-            bool is_digit, flag = false;
-            while (op('0') || op('1') || op('2') || op('3') || op('4') || op('5') || op('6') || op('7') || op('8') || op('9')) {
-                flag = true;
-            };
-            return flag;
-        }
-
-        // <factor> ::= <number> | <var> [^ <number>]* | ( <expression> )    //
-        bool factor() {
-            if (number()) return true;
-            if (var()) {
-                if(op('^') && number()) return true;
-                else return true;
+        bool number(int *value)  {
+            bool f = false;
+            *value = 0;
+            while(s[pos] == ' ') pos++;
+            while(s[pos] >= '0' && s[pos] <= '9') {
+                *value *= 10;
+                *value += s[pos] - '0';
+                pos++;
+                f = true;
             }
-
-            return rbr_l() && expression() && rbr_r();
+            return f;
         }
 
-        bool first_factor() {
-            std::cout << "first_factor" << std::endl;
-            addop();
-            return factor();
-        }
-
-        bool rest() {
-            std::cout << "rest" << std::endl;
-            while(mulop() && factor());
-            return true;
-        }
-
-        bool term() {
-            std::cout << "term" << std::endl;
-            return factor() && rest();
-        }
-
-        bool first_term() {
-            std::cout << "first_term" << std::endl;
-            return first_factor() && rest();
-        }
-
-        bool expression() {
-            if (!first_term()) return false;
-            while(addop() && term());
-            return true;
+        bool parse_x(TOK * t) {
+            int value; bool f = false;
+            if (op('x')) {
+                f = true;
+                if (op('^')) {
+                    if (number(&value)) {
+                       t->p = value;
+                    } else {
+                       std::cout << "ERROR: need number";
+                    }
+                } else {
+                    t->p = 1;
+                }
+            } else {
+                t->p = 0;
+            }
+            return f;
         }
 
         void parse() {
-            expression();
+            while(s[pos] != '\0') {
+                TOK t;
+                int value;
+
+                if (op('-')) {
+                    t.k = -1;
+                } else if (op('+')) {
+                    t.k = 1;
+                } else {
+                    t.k = 1;
+                }
+
+                if (number(&value)) {
+                    t.k *= value;
+                    if (op('*')) {
+                        parse_x(&t);
+                    } else {
+                        t.p = 0;
+                    }
+                } else {
+                    parse_x(&t);
+                }
+                toks.push_back(t);
+            }
         }
     public:
         ExParser(char * str): s(str), pos(0) {
@@ -112,17 +90,95 @@ class ExParser {
         }
         ~ExParser() {
         }
+
+        vector<TOK> get_toks() {
+            return toks;
+        }
 };
+
+
+string to_str(TOK &t, string plus) {
+    stringstream ss(ios_base::out);
+    if (t.p > 1) {
+        if (t.k > 1) {
+            ss << plus << t.k << "*x^" << t.p;
+        } else if (t.k < 0) {
+            ss << t.k << "*x^" << t.p;
+        } else {
+            ss << plus << "x^" << t.p;
+        }
+    } else if(t.p == 1){
+        if (t.k > 1) {
+            ss << plus << t.k << "*x";
+        } else if (t.k < 1) {
+            ss << t.k << "*x";
+        } else {
+            ss << plus << "x";
+        }
+    } else {
+        if (t.k >= 1) {
+            ss << plus << t.k;
+        } else if (t.k < 0) {
+            ss << t.k;
+        }
+    }
+    return ss.str();
+}
+
 
 int main(int argc, char ** argv) {
 
+/*
     for (int i = 0; i < argc; i++) {
         std::cout << i << ": " << argv[i] << endl;
     }
+*/
 
     char * str = argv[1];
 
     ExParser p(str);
+    vector<TOK> toks = p.get_toks();
+
+    std::sort(toks.begin(), toks.end(), [](TOK &a, TOK &b){ return b.p < a.p; });
+
+/*
+    for (vector<TOK>::iterator it = toks.begin(); it != toks.end(); it++) {
+        std::cout << "TOK[" << (*it).k << ", " << (*it).p << "]" << std::endl;
+    }
+    std::cout << std::endl;
+*/
+
+    for (vector<TOK>::iterator it = toks.begin(); it != toks.end(); it++) {
+        it->k *= it->p;
+        if (it->p != 0) it->p--;
+    }
+
+
+    for (vector<TOK>::iterator it = toks.begin(); it != toks.end(); it++) {
+        std::cout << "TOK[" << (*it).k << ", " << (*it).p << "]" << std::endl;
+    }
+    std::cout << std::endl;
+
+
+    vector<TOK>::iterator it, it_prev;
+    it = it_prev = toks.begin();
+    while (++it != toks.end()) {
+        if (it->p == it_prev->p) it_prev->k += it->k;
+        else {
+            it_prev++;
+            it_prev->k = it->k;
+            it_prev->p = it->p;
+        }
+    }
+    toks.resize(++it_prev - toks.begin());
+
+    stringstream ss(ios_base::out);
+    string plus = "";
+    for (vector<TOK>::iterator it = toks.begin(); it != toks.end(); it++) {
+        ss << to_str(*it, plus);
+        plus = "+";
+    }
+    std::cout << ss.str() << std::endl;
 
     return 0;
 }
